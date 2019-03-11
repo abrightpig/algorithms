0aeaf7f06bc89380bf1fe0b83a7820d113137201
liangzhu <brightpig61@gmail.com>
2019-03-11 14:16:45 +0000

    udpate insert_r

--- c/bs_tree.c
+++ c/bs_tree.c
@@ -24,13 +24,14 @@ tree_node_t *create(int key)
         return NULL;
     }
     root->key = key;
+    root->l_child = root->r_child = NULL;
     return root;
 }
 
 
 tree_node_t *insert(tree_node_t *root, int key) 
 {
-    printf("inserting: key=%d\n", key);
+    //printf("inserting: key=%d\n", key);
     if (root == NULL) {
         return create(key);
     }
@@ -62,10 +63,50 @@ tree_node_t *insert(tree_node_t *root, int key)
     return root;
 }
 
-
-tree_node_t *insert_r(tree_node_t *root, int )
+tree_node_t *insert_r(tree_node_t *root, int key)
 {
+    if (root == NULL) return NULL;
 
+    tree_node_t *tmp = NULL;
+    if (key < root->key) tmp = insert_r(root->l_child, key);
+    else if (key > root->key) tmp = insert_r(root->r_child, key);
+    else return root;
+
+    if (tmp == NULL) {
+        tree_node_t *tmp_node = (tree_node_t *)malloc(sizeof(tree_node_t));
+        if (tmp_node == NULL) {
+            fprintf(stderr, "alloc new node failed\n");
+            return NULL;
+        }
+        tmp_node->key = key;
+        tmp_node->parent = root;
+        tmp_node->l_child = tmp_node->r_child = NULL;
+        if (key < root->key) root->l_child = tmp_node;
+        else root->r_child = tmp_node;
+    }
+    return root;
+}
+
+tree_node_t *insert_r2(tree_node_t *p, int key)
+{
+    if (p == NULL) {
+        p = (tree_node_t *)malloc(sizeof(tree_node_t));
+        if (p == NULL) {
+            fprintf(stderr, "alloc new node failed\n");
+            return NULL;
+        }
+        p->key = key;
+        p->l_child = p->r_child = NULL;
+    }
+    else if (key < p->key) {
+        p->l_child = insert_r2(p->l_child, key);
+    }
+    else if (key > p->key) {
+        p->r_child = insert_r2(p->r_child, key);
+    }
+    /* if key == p->key : do nothing*/
+
+    return p;
 }
 
 void travel(tree_node_t *p)
@@ -90,30 +131,67 @@ int count(const tree_node_t *root, int key)
 }
 
 
+tree_node_t *insert_nodes(int *arr, int len)
+{
+    tree_node_t *root = NULL;
+    for (int i = 0; i < len; ++i) {
+        tree_node_t *result = insert(root, arr[i]);
+        if (result == NULL) {
+            fprintf(stderr, "create tree failed\n");
+            return NULL;
+        }
+        root = result;
+    }
+    return root;
+}
+
+
+tree_node_t *insert_nodes_r(int *arr, int len)
+{
+    tree_node_t *root = NULL;
+    for (int i = 0; i < len; ++i) {
+        /*
+        if (root == NULL) {
+            root = create(arr[i]);
+        }
+        else {
+            //root = insert_r(root, arr[i]);
+        }
+        */
+        root = insert_r2(root, arr[i]);
+        if (root == NULL) {
+            fprintf(stderr, "create tree failed\n");
+            return NULL;
+        }
+    }
+    return root;
+}
+
+
+#define ARRAY_SIZE  10
+#define RAND_RANGE  100
+
 int main() {
-    int a[10] = {0};
-    int len = 10;
+    int a[ARRAY_SIZE] = {0};
+    int len = ARRAY_SIZE;
     time_t t;
 
     /* generate random source data */
     srand((unsigned) time(&t));
     printf("source:\n");
     for (int i = 0; i < len; ++i) {
-        a[i] = (int32_t)rand() % 100;
+        a[i] = (int32_t)rand() % RAND_RANGE;
         printf("%d ", a[i]);
     }
     printf("\n");
 
 
     /* insert */
-    tree_node_t *root = NULL;
-    for (int i = 0; i < len; ++i) {
-        tree_node_t *result = insert(root, a[i]);
-        if (result == NULL) {
-            fprintf(stderr, "create tree failed\n");
-            return -1;
-        }
-        root = result;
+    //tree_node_t *root = insert_nodes(a, len);
+    tree_node_t *root = insert_nodes_r(a, len);
+    if (root == NULL) {
+        fprintf(stderr, "create tree failed\n");
+        return -1;
     }
 
     /* travel */
